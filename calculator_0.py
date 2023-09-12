37911a838535521030ae73468518733580f5d191
HelloWorld09812 <144750070+HelloWorld09812@users.noreply.github.com>
2023-09-12 23:35:12 +0300

    Create calculator_0.py

---
+++ calculator_0.py
@@ -0,0 +1,16 @@
+n_1 = int(input())
+n_2 = int(input())
+action = input()
+
+if action == '+':
+    print(n_1 + n_2)
+elif action == '-':
+    print(n_1 - n_2)
+elif action == '*':
+    print(n_1 * n_2)
+elif action == '/' and n_2 == 0:
+    print('На ноль делить нельзя!')
+elif action == '/':
+    print(n_1 / n_2)
+else:
+    print('Неверная операция')
