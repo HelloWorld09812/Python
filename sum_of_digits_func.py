24645c450505a409427e82db119c06db13533c5e
HelloWorld09812 <144750070+HelloWorld09812@users.noreply.github.com>
2023-09-25 06:57:09 +0300

    Create sum_of_digits_func.py

---
+++ sum_of_digits_func.py
@@ -0,0 +1,15 @@
+# объявление функции
+def print_digit_sum(num):
+    s = []
+    while num != 0:
+        last_digit = num % 10
+        s.append(last_digit)
+        num //= 10
+    print(sum(s))
+        
+
+# считываем данные
+num = int(input())
+
+# вызываем функцию
+print_digit_sum(num)
