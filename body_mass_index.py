2e2f2e1e976bab1ce14f73cff27ebcb1f8b05d6e
HelloWorld09812 <144750070+HelloWorld09812@users.noreply.github.com>
2023-10-26 04:55:14 +0000

    Update body_mass_index.py

--- body_mass_index.py
+++ body_mass_index.py
@@ -2,6 +2,7 @@ mass, height = float(input()), float(input())
 
 body_mass_index = mass / (height * height)
 
+
 if body_mass_index <= 25 and body_mass_index >= 18.5:
     print("Оптимальная масса")
 elif body_mass_index < 18.5:
